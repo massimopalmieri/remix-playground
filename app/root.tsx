657b928b2a0c2062d1c87f1a4d224674880d41af
Massimo Palmieri <massimopalmieri@gmail.com>
2022-11-07 17:52:07 +0000

    test

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type {LinksFunction, MetaFunction} from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,12 +7,35 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import nProgressStyles from 'nprogress/nprogress.css'
+import remixIcon from 'remixicon/fonts/remixicon.css'
+import mapboxCss from 'mapbox-gl/dist/mapbox-gl.css'
+import styles from './styles/tailwind.css'
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+
+export const links: LinksFunction = () => {
+  return [
+    {rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css'},
+    {
+      rel: 'stylesheet',
+      href: 'https://unpkg.com/file-icon-vectors@1.0.0/dist/file-icon-vivid.min.css',
+    },
+    {rel: 'stylesheet', href: remixIcon},
+    {
+      rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css',
+    },
+    {rel: 'stylesheet', href: nProgressStyles},
+    {rel: 'stylesheet', href: mapboxCss},
+    {rel: 'stylesheet', href: styles},
+  ]
+}
 
 export default function App() {
   return (
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,12 @@
 import Select from 'react-select';
 import {SelectField} from '~/components';
+import {useId} from 'react';
 
 export default function Index() {
 
   return (
-    <div>
-      <Select options={[{value: '123', label: 'hello'}]} />
+    <div className={'prose'}>
+      <Select options={[{value: '123', label: 'hello'}]} inputId={useId()} instanceId={useId()} />
       <SelectField label='hello' error={'!!!!!'} isMulti={true} options={[{value: '123', label: 'hello'}]} />
     </div>
   );
